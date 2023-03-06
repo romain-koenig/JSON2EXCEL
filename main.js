9f216f18d1967656ab56b9b68f7c935bc80ee359
Romain KOENIG <romain@kromatic.fr>
2023-03-06 14:54:19 +0100

    Looks bad but works fine

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const xl = require('excel4node');
 require('dotenv').config();
 const fetch = require('node-fetch');
+const fs = require('fs');
 
 // Remplacez les variables ci-dessous par vos propres clés et ID de table
 const apiKey = process.env.API_KEY;
@@ -9,70 +10,118 @@ const tableName = process.env.TABLE_ID;
 
 const url = `https://api.airtable.com/v0/${baseId}/${tableName}`;
 
-async function fetchData() {
+async function fetchData(offset = null, records = []) {
 	try {
-		const response = await fetch(url, {
+		let apiUrl = url;
+		if (offset) {
+			apiUrl += `?offset=${offset}`;
+		}
+		const response = await fetch(apiUrl, {
 			headers: {
 				Authorization: `Bearer ${apiKey}`,
 				'Content-Type': 'application/json',
 			},
 		});
 		const data = await response.json();
-		return data.records;
+		records.push(...data.records);
+		if (data.offset) {
+			return fetchData(data.offset, records);
+		} else {
+			return records;
+		}
 	} catch (error) {
 		console.log(error);
 	}
 }
 
+function loadJSON(filepath) {
+	return JSON.parse(fs.readFileSync(filepath));
+}
+
+function writeJSON(data, filepath) {
+	fs.writeFile(filepath, JSON.stringify(data), (err) => {
+		if (err)
+			throw err;
+		console.log('The file has been saved!');
+	});
+}
+
+const localJSON = './input/data.json';
 (async () => {
 	console.log('Here we start');
-	const data = await fetchData();
-	console.log(data);
 
-	// On crée un objet pour stocker les données filtrées par destinataire
-	const filteredData = {};
 
-	// On boucle sur les données pour filtrer par destinataire
-	data.forEach(record => {
-		const recipient = record.fields['Contact'];
-		if (!filteredData[recipient]) {
-			filteredData[recipient] = [];
-		}
-		filteredData[recipient].push(record.fields);
-	});
+	const callAPI = true;
 
-	// On boucle sur les données filtrées pour créer un fichier Excel par destinataire
-	Object.keys(filteredData).forEach(recipient => {
+	let data = null;
+
+	if (callAPI) {
+
+		data = await fetchData();
+
+		// écriture du JSON dans un fichier
+		writeJSON(data, localJSON);
+	}
+	else {
+
+		data = loadJSON(localJSON);
+	}
+
+	const recipients = data.map(record => record.fields['Contact']);
+	const uniqueRecipients = [...new Set(recipients)].sort();
+
+	// POUR TESTS : seulement une destinataire
+	// const uniqueRecipients = [];
+	// uniqueRecipients.push("John Doe");
+
+	console.log(uniqueRecipients);
+
+
+	uniqueRecipients.forEach(recipient => {
+
 		const wb = new xl.Workbook();
-		const ws = wb.addWorksheet('Worksheet Name');
+		const recipientData = data.filter(record => record.fields['Contact'] === recipient);
+
+		const ws = wb.addWorksheet(recipient.replaceAll('/', '_'));
 
 		const headingColumnNames = [
-			'GA Property',
-			'Type',
-			'Expires',
-			'Website',
-			'Owner',
-			'IT HQ Proposition',
-			'Priority Level (HQ)',
-			'GCMS',
-			'G24',
+			"GA Property",
+			"Type",
+			"Expires",
+			"Website",
+			"Owner",
+			"IT HQ Proposition",
+			"Priority Level (HQ)",
+			"GCMS",
+			"G24",
 		];
 
 		let headingColumnIndex = 1;
 		headingColumnNames.forEach(heading => {
-			ws.cell(1, headingColumnIndex++).string(heading);
+			ws.cell(1, headingColumnIndex++)
+				.string(heading)
 		});
 
-		// On boucle sur les données filtrées pour les ajouter au fichier Excel correspondant
 		let rowIndex = 2;
-		filteredData[recipient].forEach(row => {
-			let columnIndex = 1;
-			Object.keys(row).forEach(columnName => {
-				ws.cell(rowIndex, columnIndex++).string(row[columnName]);
-			});
+		recipientData.forEach(record => {
+			ws.cell(rowIndex, 1).string(record.fields["GA Property"] ? record.fields["GA Property"].toString() : "");
+			ws.cell(rowIndex, 2).string(record.fields.Type ? record.fields.Type.toString() : "");
+			ws.cell(rowIndex, 3).string(record.fields.Expires ? record.fields.Expires.toString() : "");
+			ws.cell(rowIndex, 4).string(record.fields.Website ? record.fields.Website.toString() : "");
+			ws.cell(rowIndex, 5).string(record.fields["Owner "] ? record.fields["Owner "].toString() : "");
+			ws.cell(rowIndex, 6).string(record.fields["IT HQ Proposition"] ? record.fields["IT HQ Proposition"].toString() : "");
+			ws.cell(rowIndex, 7).string(record.fields["Priority Level (HQ)"] ? record.fields["Priority Level (HQ)"].toString() : "");
+			ws.cell(rowIndex, 8).string(record.fields.GCMS ? record.fields.GCMS.toString() : "");
+			ws.cell(rowIndex, 9).string(record.fields.G24 ? record.fields.G24.toString() : "");
 			rowIndex++;
 		});
 
-		wb.write(`./output/${recipient.replace('/', '_')}.xlsx`);
+
+
+		wb.write(`./output/${recipient.replaceAll('/', '_')}.xlsx`);
 	});
 })();
+
+
+
+
